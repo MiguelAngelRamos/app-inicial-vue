94a0c0eb478ffbc0b199176e1e3a4edfcaaa61f2
Miguel Angel Ramos <return.infox@gmail.com>
2024-04-25 10:00:45 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <Navbar v-bind:titulonav="titulo"  v-bind:servicionav="servicios"/>
+  <div class="container mt-4">
+    <!-- <h1>{{ saldo }}</h1> -->
+
+    <Table v-bind:saldoTable="saldo"/>
+
+    <button v-on:click="agregarSaldo" class="btn btn-success mx-2">Aumentar Saldo</button>
+    <button v-on:click="disminuirSaldo" class="btn btn-danger">Disminuir Saldo </button>
+
+    <!-- <a v-bind:href="url">Ir Github</a> -->
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import Table from './components/Table.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Table
+  },
+  data() {
+    return {
+      saldo: 0,
+      titulo: "BancoVue",
+      servicios: ['Prestamos', 'Cuenta Corriente', 'Giros', 'Depositos'],
+      url: "https://www.github.com"
+    }
+  },
+  methods: {
+    agregarSaldo() {
+      this.saldo = this.saldo + 1;
+    },
+    disminuirSaldo() {
+      this.saldo = this.saldo - 1;
+    }
+  
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/Table.vue
@@ -0,0 +1,36 @@
+<template>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Saldo</th>
+      <th scope="col">Estado</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    <tr>
+      <td v-bind:class="colorSaldo">{{ saldoTable }}</td>
+      <td>@mdo</td>
+    </tr>
+ 
+
+  </tbody>
+</table>
+</template>
+
+
+<script>
+export default {
+  props: {
+    saldoTable: Number
+  },
+  computed: {
+    colorSaldo() {
+      return this.saldoTable >= 0 ? 'text-success': 'text-danger';
+    }
+  }
+}
+</script>
+
+<style>
+</style>
